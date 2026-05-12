3fd6adadcc04bce901bf5fa9dd112de847e5d8b5
Aayash <dangaayash@gmail.com>
2026-05-12 13:17:10 +0545

    expense routes wired in the server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js'
+import expenseRoutes from './routes/expenseRoutes.js'
 
 dotenv.config();
 connectDB();
@@ -20,6 +21,7 @@ app.use(cors({
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/expenses', expenseRoutes);
 app.get('/', (req, res) => {
     res.send('API is running ');
 })
